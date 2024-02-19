b71221643e3f1e6f563ac322a3a1a12db5c14797
Irina_La <Ireeentv@gmail.com>
2024-02-20 01:19:24 +0700

    add likes HW2

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,9 +1,21 @@
 
 fun main () {
+    //Homework 1
     val commissionSize = 0.0075
     val minCommission = 35
     val amount = 10_000
 
     var commission = if (amount*commissionSize > minCommission) amount*commissionSize else minCommission
     println(commission)
+    println()
+
+    //Homework 2
+    val likes = 141
+    val likesToString = "$likes"; //преобразуем в строку, чтобы определить окончание
+
+    if (likesToString.endsWith('1') && !likesToString.endsWith("11")) {
+        println("Понравилось " + likes + " человеку.")
+    } else {
+        println("Понравилось " + likes + " людям.")
+    }
 }
