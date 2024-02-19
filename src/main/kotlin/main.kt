c89103ea11f965113319ec9549aaacd5b9a178a3
Irina_La <Ireeentv@gmail.com>
2024-02-20 01:48:38 +0700

    add discount calculation HW3

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -11,11 +11,39 @@ fun main () {
 
     //Homework 2
     val likes = 141
-    val likesToString = "$likes"; //преобразуем в строку, чтобы определить окончание
+    val likesToString = "$likes" //преобразуем в строку, чтобы определить окончание
 
     if (likesToString.endsWith('1') && !likesToString.endsWith("11")) {
         println("Понравилось " + likes + " человеку.")
     } else {
         println("Понравилось " + likes + " людям.")
     }
+    println()
+
+    //Homework 3
+    val minSum = 1_000
+    val maxSum = 10_000
+    val discountFromMinSum = 100
+    val discountOverMaxSum = 0.05
+    val specialDiscount = 0.01
+
+    val sumOrder = 5_000 //сумма покупки
+    val permanentCustomer = true //признак постоянного покупателя
+
+    var result = sumOrder
+
+    //рассчитываем скидки от суммы покупки
+    if (sumOrder > maxSum) {
+        result = (sumOrder - (sumOrder * discountOverMaxSum)).toInt()
+    } else if (sumOrder > minSum) {
+        result = (sumOrder - discountFromMinSum)
+    }
+
+    //проверяем признак постоянного покупателя
+    if (permanentCustomer) {
+        result -= (result * specialDiscount).toInt()
+    }
+
+    println("Итоговая стоимость: " + result + " руб.")
+
 }
