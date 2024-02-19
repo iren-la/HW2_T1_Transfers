f27fae608548f6689a9563ec4a0d13f506b66406
Irina_La <Ireeentv@gmail.com>
2024-02-20 01:00:40 +0700

    add commission calculation

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,4 +1,9 @@
 
 fun main () {
+    val commissionSize = 0.0075
+    val minCommission = 35
+    val amount = 10_000
 
+    var commission = if (amount*commissionSize > minCommission) amount*commissionSize else minCommission
+    println(commission)
 }
